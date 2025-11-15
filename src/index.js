b60676835775d271c8338551c42f022f12bd9431
emmaa1009-collab <emmaa1009@gmail.com>
2025-11-15 15:36:47 +1100

    Updated cities dropdown, optimised JS

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 function updateTime() {
-  //New York
-  let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  let cities = [
+    { id: "los-angeles", timeZone: "America/Los_Angeles" },
+    { id: "johannesburg", timeZone: "Africa/Johannesburg" },
+    { id: "mumbai", timeZone: "Asia/Calcutta" },
+    { id: "tokyo", timeZone: "Asia/Tokyo" },
+    { id: "sao-paulo", timeZone: "Brazil/East" },
+    { id: "new-york", timeZone: "America/New_York" },
+    { id: "london", timeZone: "Europe/London" },
+    { id: "dubai", timeZone: "Asia/Dubai" },
+    { id: "singapore", timeZone: "Asia/Singapore" },
+    { id: "sydney", timeZone: "Australia/Sydney" },
+  ];
 
-  newYorkDateElement.innerHTML = newYorkTime.format("dddd, D MMMM, YYYY");
-  newYorkTimeElement.innerHTML = `${newYorkTime.format(
-    "h:mm:ss[<small>] A[</small>]"
-  )}`;
+  cities.forEach(({ id, timeZone }) => {
+    let cityElement = document.querySelector(`#${id}`);
+    let dateElement = cityElement.querySelector(".date");
+    let timeElement = cityElement.querySelector(".time");
+    let cityTime = moment().tz(timeZone);
 
-  //Sydney
-  let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
-
-  sydneyDateElement.innerHTML = sydneyTime.format("dddd, D MMMM, YYYY");
-  sydneyTimeElement.innerHTML = `${sydneyTime.format(
-    "h:mm:ss[<small>] A[</small>]"
-  )}`;
+    dateElement.innerHTML = cityTime.format("dddd, D MMMM, YYYY");
+    timeElement.innerHTML = cityTime.format("h:mm:ss[<small>] A[</small>]");
+  });
 }
 
 function addCity(event) {
-  let blankElement = document.querySelector("#blank-city");
-  let blankH2Element = blankElement.querySelector("h2");
-  let blankDateElement = blankElement.querySelector(".date");
-  let blankTimeElement = blankElement.querySelector(".time");
+  let cities = {
+    losAngeles: "los-angeles",
+    johannesburg: "johannesburg",
+    mumbai: "mumbai",
+    tokyo: "tokyo",
+    saoPaulo: "sao-paulo",
+  };
 
-  let cityTimeZone = event.target.value;
-  let cityName = "";
-  let cityTime = "";
+  let selected = cities[event.target.value];
 
-  if (event.target.value.length > 0) {
-    blankElement.classList.remove("hidden");
-    blankH2Element.innerHTML = cityName;
-    blankDateElement.innerHTML = `Hey`;
-    blankTimeElement.innerHTML = `You`;
-    alert(`${cityTimeZone}`);
-  } else {
-    blankElement.classList.add("hidden");
+  Object.values(cities).forEach((id) => {
+    document.querySelector(`#${id}`).classList.add("hidden");
+  });
+
+  if (selected) {
+    document.querySelector(`#${selected}`).classList.remove("hidden");
   }
 }
 
